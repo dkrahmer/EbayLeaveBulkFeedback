80f6d7edb373ddffd21ab74ad92e280f81fa1b2b
Doug Krahmer <doug.git@remhark.com>
2019-02-16 17:04:57 -0800

    Add Costura.Fody to merge all assemblies. Increment version for release.

--- EbayLeaveBulkFeedback/Properties/AssemblyInfo.cs
+++ EbayLeaveBulkFeedback/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("KrahmerSoft")]
 [assembly: AssemblyProduct("EbayLeaveBulkFeedback")]
-[assembly: AssemblyCopyright("Copyright © 2018 Doug Krahmer")]
+[assembly: AssemblyCopyright("Copyright © 2019 Doug Krahmer")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
